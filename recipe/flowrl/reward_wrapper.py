59038b4c395141ea30f9a265b98b90f2f3dc7eda
Michael Xu <michaelxu@microsoft.com>
2025-11-21 23:25:10 -0800

    fix signal issue in ray actors

--- recipe/flowrl/reward_wrapper.py
+++ recipe/flowrl/reward_wrapper.py
@@ -1,3 +1,18 @@
+import signal
+
+# Monkey-patch signal.signal to ignore errors in non-main threads (Ray actors)
+_original_signal = signal.signal
+
+def _patched_signal(signalnum, handler):
+    try:
+        return _original_signal(signalnum, handler)
+    except ValueError:
+        # Signal handlers can only be set in main thread
+        # Silently ignore this in Ray actor threads
+        pass
+
+signal.signal = _patched_signal
+
 from rllm.rewards.code_reward import rllm_reward_fn_code as compute_score
 
 # Re-export it
